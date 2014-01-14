592a801108792633ba4f945abc50dbdababa3d86
Joshua Seaton <joshuaseaton@Joshua-Seatons-MacBook-Pro.local>
2014-01-14 16:10:14 -0500

    assignment 3 exercise 4 submission

---
+++ exercise4.rb
@@ -0,0 +1,16 @@
+one_to_hundred = (1..100)
+one_to_hundred.each do |num|
+	if num % 3 ==0 && num % 5 ==0
+		puts "Bitmaker"
+	elsif num % 3 ==0
+		puts "Bit"
+	elsif num % 5 ==0
+		puts "Maker"
+	else puts "#{num}"
+	end
+end
+			
+			
+
+
+
