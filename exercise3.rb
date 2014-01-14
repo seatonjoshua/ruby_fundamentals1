0b5337747727619e6738ae085d512d22d4f44a64
Joshua Seaton <joshuaseaton@Joshua-Seatons-MacBook-Pro.local>
2014-01-14 15:18:31 -0500

    exercise3 user age and date born

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,12 @@
 puts "What is your name?"
 user_input = gets.chomp.capitalize
 puts "Hi #{user_input}!"
+
+puts "How old are you?"
+user_age = gets.chomp.to_i
+current_year = 2014
+puts "user is #{user_age}"
+year_born = current_year - user_age
+puts "user was born #{year_born} or #{year_born -1}"
+
+#exercise 3 completed above
